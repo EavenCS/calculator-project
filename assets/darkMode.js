3b1fc52aec71a26d977c0ce4937cb640277bb509
EavenCS <eaven7juni@gmail.com>
2024-07-30 18:15:46 +0200

    feat: Navbar partially completed

--- assets/darkMode.js
+++ assets/darkMode.js
@@ -1,17 +1,22 @@
-// darkMode.js
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleDarkMode = document.getElementById('toggleDarkMode');
-    const darkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleDarkModeButton = document.getElementById('toggle-dark-mode');
+    const darkModeIcon = document.getElementById('dark-mode-icon');
 
-    // Set initial mode based on localStorage
-    if (darkModeEnabled) {
+    // Load dark mode state from localStorage
+    if (localStorage.getItem('dark-mode') === 'true') {
         document.body.classList.add('dark-mode');
-        toggleDarkMode.checked = true;
+        darkModeIcon.classList.replace('fa-moon', 'fa-sun');
     }
 
-    toggleDarkMode.addEventListener('change', function() {
-        const isDarkMode = toggleDarkMode.checked;
-        document.body.classList.toggle('dark-mode', isDarkMode);
-        localStorage.setItem('darkModeEnabled', isDarkMode); // Save the mode in localStorage
+    toggleDarkModeButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+
+        if (document.body.classList.contains('dark-mode')) {
+            darkModeIcon.classList.replace('fa-moon', 'fa-sun');
+            localStorage.setItem('dark-mode', 'true');
+        } else {
+            darkModeIcon.classList.replace('fa-sun', 'fa-moon');
+            localStorage.setItem('dark-mode', 'false');
+        }
     });
 });
